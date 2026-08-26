6f11c67334096ba5ffe3df05466abe3f559ec980
hafizrana836-wq <hafizrana836@gmail.com>
2026-08-26 13:10:34 +0500

    Force redeploy to pick up CLIENT_URL

--- backend/server.js
+++ backend/server.js
@@ -132,3 +132,4 @@ async function gracefulShutdown(signal) {
 }
 process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
 process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+// redeploy trigger
